492419ef2e0e79ded21a643d9a4dae1c497a675a
yuasys <yuasys@gmail.com>
2019-06-28 17:16:08 +0900

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>CodeSandboxさんこんにちは！</h1>
+	<h2>編集してみて、なにか不思議なことに出会えるよ</h2>
+  <p>これは雛型を出発点にして、いろいろ編集してみた結果です。</p>
+	<Button />
+</main>
---
+++ Button.svelte
@@ -0,0 +1,30 @@
+<script>
+	let count = 0;
+	const button = document.querySelector("button");
+
+	function handleClick() {
+	  count += 1;
+	}
+</script>
+
+<style>
+	button {
+	  width: 110px;
+	  background: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 8px 12px;
+	  border-radius: 2px;
+	}
+	button:focus {
+	  outline: none;
+	  cursor: pointer;
+	}
+	button:hover {
+	  cursor: pointer;
+	}
+</style>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
